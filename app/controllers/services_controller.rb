833d68a2f7fcde7ecb4e6f7a5932ba4b81b5141d
github username <github email address>
2018-09-08 10:59:01 -0400

    services are able to be created with partial form and new and then displayed on index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,4 +4,20 @@ class ServicesController < ApplicationController
     @service = Service.new
   end
 
+  def create
+    @service = Service.new(service_params)
+    @service.save
+    redirect_to services_path
+  end
+
+  def index
+    @services = Service.all 
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :description, :category, :price, :duration)
+  end
+
 end
